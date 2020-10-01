e701b64efaf89fe535f3a959fafa45f239aaf434
FDC4000 <53172803+FDC4000@users.noreply.github.com>
2020-10-01 23:47:27 +0100

    Change workingdir explicit in start script

--- monitor.sh
+++ monitor.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
-sudo python3 /usr/bin/PiDisplay/monitor.py >> /usr/bin/PiDisplay/monitor.log 2>&1
+cd /usr/bin/PiDisplay/
+sudo python3 ./monitor.py >> monitor.log 2>&1
+#sudo python3 /usr/bin/PiDisplay/monitor.py >> /usr/bin/PiDisplay/monitor.log 2>&1
